87fd4f7be19053e8ade3f1d050cccf84ecd47e46
GoncaloFernandes8 <goncalo8fernandes8@gmail.com>
2025-08-19 15:48:36 +0100

    init: adiciona projeto Spring Boot

---
+++ src/main/java/barbershopAPI/barbershopAPI/BarbershopApiApplication.java
@@ -0,0 +1,13 @@
+package barbershopAPI.barbershopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/barbershopAPI/barbershopAPI/BarbershopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package barbershopAPI.barbershopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
